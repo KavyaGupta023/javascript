cb5da048bcce15a49adbc8c45d2c742c0bb78c4c
KavyaGupta023 <kavyagupta967@gmail.com>
2025-12-24 13:46:46 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"            // this will treat all the code as new js version
+//alert("this is a mess")
+let num=56
+let str="ajay"
+let init=false
+
+//PRIMITIVE DATA TYPES
+
+//number
+//bigInt
+//string
+//boolean
+//null       =>standalone value    (special data type)     (representation hai empty value ka )
+//undefined  =>value define nhi ki     (ex:     let city)  (variable declare kara hai pr value nhi di hai )
+//symbol     =>uniqeness
+
+//NON PRIMITIVE DATA TYPES
+ 
+//object
+
+
+
+console.log(typeof "kavya");               //  string
+console.log(typeof num);                   //  number
+console.log(typeof null);                  //  object
+console.log(typeof undefined);             //  undefined
+
+
+
