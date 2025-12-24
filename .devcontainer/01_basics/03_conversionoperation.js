7d427be3cc7c25c89f4d0a6a123526d3aa3e0e92
KavyaGupta023 <kavyagupta967@gmail.com>
2025-12-24 14:40:55 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperation.js
@@ -0,0 +1,5 @@
+let score="33"
+console.log(typeof score);                    //string
+let valueInNumber=Number(score)               //data type conversion
+console.log(typeof valueInNumber);            //number
+
