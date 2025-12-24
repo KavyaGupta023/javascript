178e356c1732bddc358d3e6f483b3eab3e2c88e2
KavyaGupta023 <kavyagupta967@gmail.com>
2025-12-24 10:31:40 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+
+const accountId=144455                                           //value cannot change
+let accountEmail="kavya@gmail.com"
+var accountPassword="1234"
+accountCity="Agra"
+accountEmail="kg@gmail"                                                      //allowed
+accountPassword="222"                                                        //allowed
+accountCity="jaipur"                                                         //allowed
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])          //print in tabular form
