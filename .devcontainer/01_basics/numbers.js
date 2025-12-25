316e3173197356e864ec3a0ffe8e9c4d4cefa0b5
KavyaGupta023 <kavyagupta967@gmail.com>
2025-12-25 16:41:11 +0000

    number and string examples

---
+++ .devcontainer/01_basics/numbers.js
@@ -0,0 +1,10 @@
+const num=123
+const n=new Number(234.8565)
+console.log(num);
+console.log(n);
+console.log(n.toString());
+console.log(n.toFixed(2));
+console.log(n.toPrecision(4));
+const n2=1000500
+console.log(n2.toLocaleString());
+
---
+++ .devcontainer/01_basics/strings.js
@@ -0,0 +1,17 @@
+const name="kavya"
+const age=20
+console.log(`my name is ${name} and age is ${age}`);
+const data=new String('kavya-is-mad')
+console.log(data[2]);
+console.log(data.length);
+console.log(data.charAt(4));
+console.log(data.indexOf('a'));
+console.log(data.split('-'));
+console.log(data.toUpperCase());
+console.log(data);
+
+
+
+
+
+
