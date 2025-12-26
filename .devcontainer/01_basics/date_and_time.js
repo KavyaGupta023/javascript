36f7d8db250d8ca41db0305166bbce4687cddf35
KavyaGupta023 <kavyagupta967@gmail.com>
2025-12-26 11:55:37 +0000

    date and time basics

--- .devcontainer/01_basics/date_and_time.js
+++ .devcontainer/01_basics/date_and_time.js
@@ -1,6 +1,15 @@
-const date=new Date()
-console.log(date);
-console.log(date.toString());
-console.log(date.toLocaleString());
+const date=new Date()                              //date ka typeof will return 'object'
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toLocaleString());
+// console.log(date.toDateString());
+// const d1=new Date(2027,5,23)
+// console.log(d1.toDateString());
+// const t=Date.now();
+// console.log(t);
 
+console.log(date.toLocaleString('default',{
+    weekday: "long",
+   
+}));
 
