d569b599df9e2202edc68264e9887f0e4fc9bcb2
KavyaGupta023 <kavyagupta967@gmail.com>
2025-12-27 18:01:34 +0000

    functions and scopes basics

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -6,8 +6,42 @@
 //     console.log("A")
 // }
 
-function add( n1,n2){
-    console.log(n1+n2)
-}
+// function add(n1,n2){                            //parameters ka type dene ki zarurat nhi hai
+//     console.log(n1+n2)
+// }
 
-add(6,7.9)
+// const x=add(6,"ao")
+// console.log("result" ,x );
+
+// function func(name="sam"){
+//     return `name is ${name}`
+// }
+// console.log(func())
+
+// function calculateCartPrice(val1,val2,...num1){
+//     return num1
+// }
+// console.log(calculateCartPrice(4,5,7,8,9));
+
+// const obj={
+//     name:"kavya",
+//     age:20,
+//     mail:"kavya@mail.com"
+// }
+
+// function data(user){
+//         console.log("name is",user.name);
+        
+// }
+
+// data({
+//     name:"kavya",
+//     age:20,
+//     mail:"kavya@mail.com"
+// })
+
+const arr=[100,200,300,400]
+function f1(array){
+    return array[2]
+}
+console.log(f1(arr));
---
+++ .devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,23 @@
+// if(true){
+//     let a=10
+//     const b=20
+//     var c=30
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const name="kavya"
+
+    function two(){
+        const age=20
+        console.log(name);
+        
+    }
+    console.log(age)
+    two()
+}
+
+one()
