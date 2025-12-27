0dfa435f48e7cedc8629ce63453a8363aa4ebf2b
KavyaGupta023 <kavyagupta967@gmail.com>
2025-12-27 13:44:37 +0000

    func

---
+++ .devcontainer/03_basics/01_functions.js
@@ -0,0 +1,13 @@
+// function sayName(){
+//     console.log("K")
+//     console.log("A")
+//     console.log("V")
+//     console.log("Y")
+//     console.log("A")
+// }
+
+function add( n1,n2){
+    console.log(n1+n2)
+}
+
+add(6,7.9)
