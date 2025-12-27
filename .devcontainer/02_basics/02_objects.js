1923c86f4b00770f9a7f41c7fe14dcf05d462414
KavyaGupta023 <kavyagupta967@gmail.com>
2025-12-27 08:53:05 +0000

    objects more

--- .devcontainer/02_basics/02_objects.js
+++ .devcontainer/02_basics/02_objects.js
@@ -5,19 +5,54 @@
 //         "full name": "kavya gupta"
 //      }
 
-// console.log(data.name);
-// console.log(data["age"]);
+// data.student=false                      //overwriting
+// data.gmail="kavyagupta@gmail.com"         
+
+// console.log(data.student);
+// console.log(data["gmail"]);
 // console.log(data.full name)               //error
 // console.log(data["full name"]);
+
+
+// data.greeting=function(){
+//         console.log("hey beautiful")
+// }   
+// console.log(data.greeting())
+
+// data.greetingTwo=function(){
+//         console.log(`the name is ${this.name} and mail is ${this.gmail}`)
+// }
+// console.log(data.greetingTwo())
  
-let mySym=Symbol("key")
-let d1={
-        name:"kavya",
-        [mySym]:"key1"
-}
-console.log(d1[mySym])
 
+// let mySym=Symbol("key")
+// let d1={
+//         name:"kavya",
+//         [mySym]:"key1"
+// }
+// console.log(d1[mySym])
 
+//***************************************************************************//
+
+// const data=new Object()
+// data.id="123abc"
+// data.name="Sam"
+
+// let data={
+//         userId:"123abc",
+//         extraDetails:{                              //nested object
+//                 fullName:{                          //another nested object
+//                         firstName:"kavya",
+//                         lastName:"gupta"
+//                 }
+//         }
+// }
+// console.log(data.extraDetails.fullName?.middleName?.id);
+
+const obj1= {1:"a" , 2:"b"}
+const obj2= {3:"c" , 4:"d"}
+const obj3= Object.assign({},obj1,obj2)
+console.log(obj3);
 
 
 
