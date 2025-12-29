56571dbe2117593b2b90f7575607b429846aac3d
KavyaGupta023 <kavyagupta967@gmail.com>
2025-12-29 08:26:30 +0000

    more abt iterations

--- .devcontainer/05_iterations/one.js
+++ .devcontainer/05_iterations/one.js
@@ -1,13 +1,13 @@
-// const arr=[1,2,3,4]
-// for (const i of "kavya") {
+//
+// for (const i of arr) {
 //     console.log(i);
     
 // }
 
-const map=new Map()
-map.set(1,"a")
-map.set(2,"b")
-map.set(3,"c") 
+// const map=new Map()
+// map.set(1,"a")
+// map.set(2,"b")
+// map.set(3,"c") 
 
 // console.log(map);
 
@@ -16,7 +16,46 @@ map.set(3,"c")
     
 // }
 
-for (const [k,v] of map) {
-    console.log(k,"=>",v);
-}
+// for (const [k,v] of map) {
+//     console.log(k,"=>",v);
+// }
+
+
+// const obj={
+//     1:"a",
+//     2:"b",
+//     3:"c"
+// }
+// for (const [k,v] of obj) {
+//        console.log(k,v);
+//     }
+
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key,element);
+// }
+
+// for (const i in arr) {
+//   console.log(i);
+  
+    
+// }
+
+//   const arr=[1,2,3,4]
+// arr.forEach( function (item) {
+//         console.log(item)
+// })
+
+// arr.forEach( (item)=> {
+//         console.log(item);
+        
+// })
+
+
+// function one(val){
+//     console.log(val);
+    
+// }
+// arr.forEach(one)
 
