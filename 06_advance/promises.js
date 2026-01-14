871d221b6f6d186e38d39c5ab747e86b14ee221c
KavyaGupta023 <kavyagupta967@gmail.com>
2026-01-14 16:05:37 +0000

    more abt promises

--- 06_advance/promises.js
+++ 06_advance/promises.js
@@ -31,18 +31,18 @@
 //     console.log(user);
 // })
 
-const promiseFour=new Promise(function(resolve,reject){
-    setTimeout(function(){
-        let error=true;
-        if(!error){
-            resolve({username:'kavya',password:123})
-        }
-        else{
-            reject('ERROR:went something wrong');
-        }
+// const promiseFour=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error=false;
+//         if(!error){
+//             resolve({username:'kavya',password:123})
+//         }
+//         else{
+//             reject('ERROR:went something wrong');
+//         }
         
-    },1000)
-})
+//     },1000)
+// })
 
 // const nn=promiseFour.then( (user)=>{                  //this is not allowed !!!! means storing in `nn` is not allowed
 //         console.log(user);
@@ -54,11 +54,49 @@ const promiseFour=new Promise(function(resolve,reject){
 //then how this is done
 //through chaining 
 
-promiseFour.then( (user)=>{                  //this is not allowed !!!! means storing in `nn` is not allowed
-        console.log(user);
-        return user.username;
+// promiseFour.then( (user)=>{                  //this is not allowed !!!! means storing in `nn` is not allowed
+//         console.log(user);
+//         return user.username;
         
-}).then( (username)=>{
-    console.log(username);
-})                           // iss vale then ko function mei vo value return hogi jo upar vale then se aayi hai
+
+// }).then( (username)=>{                      // iss vale then ko vo value return hogi jo upar vale then se aayi hai
+//     console.log(username);
+// }).catch(function(error){                   // catch is related to reject
+//     console.log(error);
+// }).finally(()=>{                           //will always run
+//     console.log("the promise is either resolved or rejected")})
+
+
+const promiseFive=new Promise(function(resolve,reject){
+    let error=true;
+    if(!error){
+        resolve({username:'kavya',password:'1230455'});
+    }
+    else{
+        reject('ERROR:JS went wrong');
+    }
+})
+
+//it is not important that we do promise only with .then or .catch . We can also do with async and await 
+
+// async function consumepromiseFive() {
+//     const response=await promiseFive;
+//     console.log(response);
+    
+// }
+
+ 
+async function consumepromiseFive(){                 //better than above method 
+    try{
+        const response=await promiseFive;
+        console.log(response);
+        
+    }
+    catch(error){
+        console.log(error);
+        
+    }
+}
+consumepromiseFive();
+
 
